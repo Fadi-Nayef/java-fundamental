decb0c8e6cba92d7170871aae0b08d87f4e54782
Fadi_Nayef <adraf38@hotmail.com>
2021-06-30 03:27:02 +0300

    lab 02

--- basiclibrary/lib/src/test/java/basiclibrary/LibraryTest.java
+++ basiclibrary/lib/src/test/java/basiclibrary/LibraryTest.java
@@ -4,11 +4,26 @@
 package basiclibrary;
 
 import org.junit.jupiter.api.Test;
+
+import static basiclibrary.RollDice.*;
 import static org.junit.jupiter.api.Assertions.*;
+import java.util.ArrayList;
+
 
 class LibraryTest {
     @Test void someLibraryMethodReturnsTrue() {
         Library classUnderTest = new Library();
         assertTrue(classUnderTest.someLibraryMethod(), "someLibraryMethod should return 'true'");
     }
-}
+    @Test public void RollDiceTest() {
+        Library libraryForTesting = new Library();
+
+        int input = 4;
+        int[] expeected = new int[input];
+        int expectedlng=expeected.length;
+
+
+        assertEquals("Length of Array should be 5", expectedlng,RollDice.roll(4));
+        assertEquals("Length of Array should be 56", testMEAgain.size(), 56);
+
+    }
